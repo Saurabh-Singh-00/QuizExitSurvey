4f5a0e25af5182652549337c0df72b6ebb6f4427
Vean <vedangnaik0812@gmail.com>
2019-02-10 18:07:46 +0530

    trying static 9

--- Quizzes_Surveys/settings.py
+++ Quizzes_Surveys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pn+1l^$&iwxh(v*p+4g7)w!tg(6(z02g1ilk5llv$+ksf@pzw!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://terna-quiz-survey.herokuapp.com/', "*"]
 
